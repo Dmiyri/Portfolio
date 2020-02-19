006b87271235d99a2e0178a752c0a50ffa028842
Dmiyri <timmy.1989.89@gmail.com>
2020-02-19 20:57:27 +0300

    first comit

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="porfolio">
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
