493f65343ee27e76cb1258dbe7608cda49abf1f0
Dmiyri <timmy.1989.89@gmail.com>
2020-02-21 20:03:51 +0300

    Updates

---
+++ precache-manifest.6e02acac6f9080d8cb452623d22215e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c9a3c90c2a3e8b611f98c035e70dd",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b2af8706643f214c84a9",
+    "url": "/Portfolio/static/css/main.86303d7b.chunk.css"
+  },
+  {
+    "revision": "1e5f430390b1c545d9b2",
+    "url": "/Portfolio/static/js/2.37d850d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Portfolio/static/js/2.37d850d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2af8706643f214c84a9",
+    "url": "/Portfolio/static/js/main.cfbb8331.chunk.js"
+  },
+  {
+    "revision": "177aa61725fb9ffd782d",
+    "url": "/Portfolio/static/js/runtime-main.f5ee5aaf.js"
+  }
+]);
